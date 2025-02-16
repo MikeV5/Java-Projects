3e992ef946fc3f95c06193d054e6f77d4610c51e
miguel <miguel142v@gmail.com>
2025-02-16 17:14:47 +0100

    Finished exercise

--- TrainingCode/PhoneNumber.java
+++ TrainingCode/PhoneNumber.java
@@ -1,3 +1,23 @@
 public class PhoneNumber {
-    
+
+    public static void main(String[] args){
+        //Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+        System.out.println(createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder phoneNumber = new StringBuilder();
+        StringBuilder stringNumbers = new StringBuilder();
+        if(numbers.length != 10) throw new IllegalArgumentException("Length must be 10");
+        for (int i : numbers) {
+            stringNumbers.append(String.valueOf(i));
+        }
+        phoneNumber.append("(");
+        phoneNumber.append(stringNumbers.substring(0, 3));
+        phoneNumber.append(") ");
+        phoneNumber.append(stringNumbers.substring(3, 6));
+        phoneNumber.append("-");
+        phoneNumber.append(stringNumbers.substring(6, 10));
+        return phoneNumber.toString();
+      }
 }
